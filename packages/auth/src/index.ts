8edd657aeef925a294d530f5c5b206cd40c81bf8
Dinihz <lucas87diniz@gmail.com>
2025-10-01 22:09:22 -0300

    feat: added permissions by position and subject type

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -5,22 +5,26 @@ import {
   MongoAbility,
   AbilityBuilder,
 } from '@casl/ability'
+import { User } from './models/user'
+import { permissions } from './permissions'
+import type { UserSubject } from './subjects/user'
+import type { ProjectSubject } from './subjects/project'
 
-const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
-type appAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+type appAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
 export type AppAbility = MongoAbility<appAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found.`)
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
