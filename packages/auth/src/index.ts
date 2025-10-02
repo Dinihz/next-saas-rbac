6e6e6820c3d337c4aa2cef8eec125b88c6a2a755
Dinihz <lucas87diniz@gmail.com>
2025-10-02 09:11:33 -0300

    chore: i optimized permission types

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,16 +1,30 @@
 import {
   createMongoAbility,
-  ForcedSubject,
   CreateAbility,
   MongoAbility,
   AbilityBuilder,
 } from '@casl/ability'
+import { z } from 'zod'
+
 import { User } from './models/user'
 import { permissions } from './permissions'
-import type { UserSubject } from './subjects/user'
-import type { ProjectSubject } from './subjects/project'
+import { userSubject } from './subjects/user'
+import { projectSubject } from './subjects/project'
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
 
-type appAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type appAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<appAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,8 +1,8 @@
 import { AbilityBuilder } from '@casl/ability'
+
 import { AppAbility } from '.'
 import { User } from './models/user'
-
-type Role = 'ADMIN' | 'MEMBER'
+import { Role } from './roles'
 
 type PermissionsByRole = (
   user: User,
@@ -14,7 +14,8 @@ export const permissions: Record<Role, PermissionsByRole> = {
     can('manage', 'all')
   },
   MEMBER(_, { can }) {
-    can('invite', 'User')
     can('manage', 'Project')
+    can('transfer_ownership', 'Organization')
   },
+  BILLING() {},
 }
