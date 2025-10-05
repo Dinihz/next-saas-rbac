ae515e39ed2605c68d608611143e1fe5c507575e
Dinihz <lucas87diniz@gmail.com>
2025-10-05 16:00:24 -0300

    feat: add remove and update members route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -31,6 +31,8 @@ import { getProject } from './routes/projects/get-project'
 import { getProjects } from './routes/projects/get-projects'
 import { updateProject } from './routes/projects/update-project'
 import { getMembers } from './routes/members/get-members'
+import { updateMember } from './routes/members/update-members'
+import { removeMember } from './routes/members/remove-member'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -90,6 +92,8 @@ app.register(getProjects)
 app.register(updateProject)
 
 app.register(getMembers)
+app.register(updateMember)
+app.register(removeMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
